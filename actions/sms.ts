2886f66ca79862538707c273883589762d5d7d33
reza_rahem <reza_rahem224@gmail.com>
2023-10-18 18:53:19 +0330

    wrap up

--- actions/sms.ts
+++ actions/sms.ts
@@ -18,38 +18,38 @@ export async function OTP(phone: string) {
   };
 }
 
-export async function simpleSms() {
-  const res = await axios.post(
-    'https://console.melipayamak.com/api/send/simple/8a0ad3e4276344c493ef1abc5c102d16',
-    {
-      from: '50004001753061',
-      to: '09384264057',
-      text: 'تلگرام',
-    }
-  );
+// export async function simpleSms() {
+//   const res = await axios.post(
+//     'https://console.melipayamak.com/api/send/simple/8a0ad3e4276344c493ef1abc5c102d16',
+//     {
+//       from: '50004001753061',
+//       to: '09384264057',
+//       text: 'تلگرام',
+//     }
+//   );
 
-  const { data, status, statusText } = res;
-  return {
-    data,
-    status,
-    statusText,
-  };
-}
+//   const { data, status, statusText } = res;
+//   return {
+//     data,
+//     status,
+//     statusText,
+//   };
+// }
 
-export async function advanceSms() {
-  const res = await axios.post(
-    'https://console.melipayamak.com/api/send/advanced/8a0ad3e4276344c493ef1abc5c102d16',
-    {
-      from: '50004001753061',
-      to: ['09384264057', '09903776525'],
-      text: '',
-    }
-  );
+// export async function advanceSms() {
+//   const res = await axios.post(
+//     'https://console.melipayamak.com/api/send/advanced/8a0ad3e4276344c493ef1abc5c102d16',
+//     {
+//       from: '50004001753061',
+//       to: ['09384264057', '09903776525'],
+//       text: '',
+//     }
+//   );
 
-  const { data, status, statusText } = res;
-  return {
-    data,
-    status,
-    statusText,
-  };
-}
+//   const { data, status, statusText } = res;
+//   return {
+//     data,
+//     status,
+//     statusText,
+//   };
+// }
